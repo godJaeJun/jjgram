a413f7e1c7d92841667c64f41e79b2fec99590a4
이재준 <asapz@naver.com>
2018-12-27 17:43:57 +0900

    create followers,following

--- jjgram/users/models.py
+++ jjgram/users/models.py
@@ -16,13 +16,18 @@ class User(AbstractUser):
         ('not-specified','Not specified')
     )
 
+    #null=True는 현재 변화한 데이터 이전의 올드 멤버에게 디폴트로 null값을 주는 것
+    #max_length는 길이 제한을 두고, choices는 선택가능하게 한다.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     website=models.URLField(null=True)
     bio=models.TextField(null=True)
     phone=models.CharField(max_length=140,null=True)
     gender=models.CharField(max_length=80,choices=GENDER_CHOICES,null=True)
-    #null=True는 현재 변화한 데이터 이전의 올드 멤버에게 디폴트로 null값을 주는 것
-    #max_length는 길이 제한을 두고, choices는 선택가능하게 한다.
+    #followers생성 자기 자신과 연결 "self"
+    followers=models.ManyToManyField("self")
+    #following생성 여러 유저가 팔로잉 할 수 있으니까 self로 설정한다.
+    following=models.ManyToManyField("self")
+
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
 
