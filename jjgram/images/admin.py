8bfa5505d50a04486b248f5c0294d05dc663b068
이재준 <asapz@naver.com>
2018-12-27 17:58:43 +0900

    create admin pannel

--- jjgram/images/admin.py
+++ jjgram/images/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+#클래스는 모델들이 어드민패널에서 어떻게 보이게 될지 결정한다.
+#데코레이터는 항상 클래스 위에 적어야 한다. 안그러면 에러 발생...
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass 
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
