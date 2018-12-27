3a4d35137c319ce1c9ed21bc8b66b1d656dcd15c
이재준 <asapz@naver.com>
2018-12-27 15:36:18 +0900

    create user modles

--- jjgram/users/models.py
+++ jjgram/users/models.py
@@ -1,14 +1,25 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
-
+#AbstractUser를 상속 받기 때문에 모든 필드를 정의하지 않아도 된다.
 class User(AbstractUser):
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    #GENDER_CHOICES를 통해서 성별을 제한을 둔다.
+    GENDER_CHOICES=(
+        ('male','Male'),
+        ('female','Female'),
+        ('not-specified','Not specified')
+    )
 
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website=models.URLField(null=True)
+    bio=models.TextField(null=True)
+    phone=models.CharField(max_length=140,null=True)
+    gender=models.CharField(max_length=80,choices=GENDER_CHOICES,null=True)
+    #null=True는 현재 변화한 데이터 이전의 올드 멤버에게 디폴트로 null값을 주는 것
+    #max_length는 길이 제한을 두고, choices는 선택가능하게 한다.
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
