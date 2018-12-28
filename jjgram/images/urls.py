6080f30c4f535210d88dbbe2622783e8e424972a
이재준 <asapz@naver.com>
2018-12-28 14:44:16 +0900

    Viewing all the Comments and Likes

--- jjgram/images/urls.py
+++ jjgram/images/urls.py
@@ -5,4 +5,6 @@ app_name="images"
 #장고 2. 이상부터는 path를 사용한다. 밑에는 /all로 들어갈 경우 view는 클래스이기 때문에 as_view로 뷰를 보여준다. 
 urlpatterns = [
     path("all/",view=views.ListAllImages.as_view(),name="all_images"),  
+    path("comments/",view=views.ListAllComments.as_view(),name="all_comments"),  
+    path("likes/",view=views.ListAllLikes.as_view(),name="all_likes"),  
 ]
--- jjgram/images/views.py
+++ jjgram/images/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response#엘리먼트를 가져오고 보여주고 method를 관리하는 클래스
 from . import models,serializers
-#TEST
+
+#TEST 모든 이미지 불러오기
 class ListAllImages(APIView):
     #APIView는 get이나 post나 사용자가 요청한대로 실행하는 능력을 가지고 있다.
     def get(self,request,format=None):#request = 클라이언트에게서 오브젝트를 요청하는 것
@@ -10,4 +11,24 @@ class ListAllImages(APIView):
         #이미지 시리얼 라이저는 단수다. 1개의 이미지를 시리얼 라이징하니까 그래서 한개가 아니라고 알려줘야함 many로!
         serializer= serializers.ImageSerializer(all_images,many=True)
         #return으로 인해 function은 종료된다. 시리얼라이즈의 데이터를 반환해라 
+        return Response(data=serializer.data)
+
+#TEST 모든 댓글 불러오기
+class ListAllComments(APIView):
+    def get(self,request,format=None):
+
+        all_comments=models.Comment.objects.all()
+
+        serializer=serializers.CommentSerializer(all_comments,many=True)
+
+        return Response(data=serializer.data)
+
+#TEST 모든 좋아요 불러오기
+class ListAllLikes(APIView):
+    def get(self,request,format=None):
+
+        all_likes=models.Like.objects.all()
+
+        serializer=serializers.LikeSerializer(all_likes,many=True)
+
         return Response(data=serializer.data)
