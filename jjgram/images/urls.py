4be32ba54d878d648ba552f36865e66392cd547e
이재준 <asapz@naver.com>
2018-12-28 21:26:54 +0900

    Using Variables on the URLS

--- jjgram/images/urls.py
+++ jjgram/images/urls.py
@@ -5,4 +5,5 @@ app_name="images"
 #장고 2. 이상부터는 path를 사용한다. 밑에는 /all로 들어갈 경우 view는 클래스이기 때문에 as_view로 뷰를 보여준다. 
 urlpatterns = [
     path("",view=views.Feed.as_view(),name="feed"), 
+    path("<int:image_id>/like",view=views.LikeImage.as_view(),name="like_image")
 ]
