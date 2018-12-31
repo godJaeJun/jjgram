6443e29d7d8bdaa21f30d5052963c02075c218fb
이재준 <asapz@naver.com>
2018-12-31 11:46:46 +0900

    Setting up JWT

--- config/settings/base.py
+++ config/settings/base.py
@@ -252,3 +252,14 @@ SOCIALACCOUNT_ADAPTER = 'jjgram.users.adapters.SocialAccountAdapter'
 # Your stuff...
 # ------------------------------------------------------------------------------
 TAGGIT_CASE_INSENSITIVE=True #대소문자 구분안하고 검색할거라는 뜻
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
