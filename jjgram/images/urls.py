b5424b4b09f70d8479b11374e33c3744ef260e50
이재준 <asapz@naver.com>
2018-12-31 15:58:51 +0900

    uploading a photo

--- jjgram/images/urls.py
+++ jjgram/images/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name="images"
 #장고 2. 이상부터는 path를 사용한다. 밑에는 /all로 들어갈 경우 view는 클래스이기 때문에 as_view로 뷰를 보여준다. 
 urlpatterns = [
-    path("",view=views.Feed.as_view(),name="feed"), 
+    path("",view=views.Images.as_view(),name="feed"), 
     path("<int:image_id>/",view=views.ImageDetail.as_view(),name="detail_image"), 
     path("<int:image_id>/likes",view=views.LikeImage.as_view(),name="like_image"),
     path("<int:image_id>/unlikes",view=views.UnLikeImage.as_view(),name="like_image"),
--- jjgram/images/views.py
+++ jjgram/images/views.py
@@ -7,7 +7,7 @@ from jjgram.users import models as user_models
 from jjgram.notifications import views as notification_views #상황에 맞게 알림이 떠야하기 때문에 가지고옴.
 
 
-class Feed(APIView):
+class Images(APIView):
     def get(self,request,format=None):
         user=request.user #요청한 유저명
 
@@ -36,6 +36,20 @@ class Feed(APIView):
         serializer=serializers.ImageSerializer(sorted_list,many=True)
 
         return Response(serializer.data)
+    
+    def post(self,request,format=None):
+        
+        user=request.user
+        
+        serializer=serializers.InputImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user)
+
+            return Response(data=serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 #좋아요
 class LikeImage(APIView):
