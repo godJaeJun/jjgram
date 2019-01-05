4303a91b0b2920c4af7ce3ee3487c3089f02a227
이재준 <asapz@naver.com>
2019-01-05 10:41:20 +0900

    Changing the NODE_PATH

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import style from './App.module.scss';
+import style from 'App.module.scss';
 
 class App extends Component {
   render() {
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import {createStore,combineReducers} from "redux";
-import users from './modules/users';
+import users from 'redux/modules/users';
 
 //합치기 첫번째 리듀서는 유저 여기서 더많은 리듀서를 작성하면 된다. 여기에 라우팅 미들웨어 등도 많이 입력할 것.
 const reducer = combineReducers({
