e8e1faab373030742abc98b8b5e3bd33abe55d6e
이재준 <asapz@naver.com>
2019-01-10 10:13:54 +0900

    create-react-app-v2

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/ie11'; // For IE 11 support
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
