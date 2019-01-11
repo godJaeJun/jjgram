1ea69eaf4ea7f60e0332eaf87b1621bc530e9558
이재준 <asapz@naver.com>
2019-01-11 10:03:01 +0900

    add package

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,13 @@
-import {createStore,combineReducers} from "redux";
+import {createStore,combineReducers,applyMiddleware} from "redux";
+import thunk from "redux-thunk"
 import user from 'redux/modules/user';
+
+const middlewares=[thunk];
 
 const reducer=combineReducers({
     user
 })
 
-let store=initalState=>createStore(reducer);
+let store=initalState=>createStore(reducer,applyMiddleware(...middlewares));
 
 export default store();
