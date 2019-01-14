596821e91ccd29177bdb334fbb0517ba646d803f
이재준 <asapz@naver.com>
2019-01-14 11:15:37 +0900

    Moving the AuthForm to their own components

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import { LoginForm, SignupForm } from "components/AuthForms";
+import PropTypes from "prop-types";
+import LoginForm from "components/LoginForm";
+import SignupForm from "components/SignupForm";
 
 const Auth = (props, context) => (
   <main className={styles.auth}>
@@ -16,23 +18,23 @@ const Auth = (props, context) => (
       <div className={styles.whiteBox}>
         {props.action === "signup" && (
           <p className={styles.text}>
-            Have an account?{" "}
+            {context.t("Have an account?")}{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
-              Log in
+            {context.t("Log in")}
             </span>
           </p>
         )}
         {props.action === "login" && (
           <p className={styles.text}>
-            Don't have an account?{" "}
+            {context.t("Don't have an account?")}{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
-              Sign up
+            {context.t("Sign up")}
             </span>
           </p>
         )}
       </div>
       <div className={styles.appBox}>
-        <span>Get the app</span>
+        <span>{context.t("Get the app")}</span>
         <div className={styles.appstores}>
           <img
             src={require("images/ios.png")}
@@ -47,4 +49,7 @@ const Auth = (props, context) => (
     </div>
   </main>
 );
+Auth.contextTypes={
+  t:PropTypes.func.isRequired
+}
 export default Auth;
