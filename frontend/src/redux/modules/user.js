e2a813e37107045b3ce744a9bc08718479631130
이재준 <asapz@naver.com>
2019-01-15 17:26:00 +0900

    re

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -1,8 +1,17 @@
 //import
 
 //actions
+//토큰을 저장시켜줌.
+const SAVE_TOKEN="SAVE_TOKEN";
 
 //action creators
+//token=내가 저장하고 싶은 토큰
+function saveToken(token){
+    return{
+        type:SAVE_TOKEN,
+        token
+    }
+}
 
 //API actions
 
@@ -18,7 +27,12 @@ function facebookLogin(access_token){
             }) 
         })
         .then(response=>response.json())//작업이 완료된 후 response입력.
-        .then(json=>console.log(json))
+        .then(json=>{
+            if(json.token){
+                localStorage.setItem("jwt",json.token)
+                dispatch(saveToken(json.token));
+            }
+        })
         .catch(err=>console.log(err));
     };
 }
@@ -27,17 +41,26 @@ function facebookLogin(access_token){
 const initialState={
     //localStorage란 브라우저에 저장하는 쿠키 같은 것. jwt가 없으면 false
     //redux thunk의 역할 : api에 request로 아이디와 비번을 보내서 맞으면 true로 변하게하고 틀리면 false로 유지시키는 역할
-    isLoggedIn:localStorage.getItem("jwt")||false
+    isLoggedIn:localStorage.getItem("jwt")?true:false
 };
 //reducer
 function reducer(state=initialState,action){
     switch(action.type){
+        case SAVE_TOKEN:
+            return applySetToken(state,action);
         default:
             return state;
     }
 }
 //reducer functions
-
+function applySetToken(state,action){
+    const {token}=action;
+    return{
+        ...state,
+        isLoggedIn:true,
+        token
+    }
+}
 //exports
 
 const actionCreators={
