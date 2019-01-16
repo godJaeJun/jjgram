0a9c609ce5280f3c85a89e1991b328d178c5e4cb
이재준 <asapz@naver.com>
2019-01-16 16:34:27 +0900

    css!

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Textarea from "react-textarea-autosize";
 import styles from "./styles.module.scss";
 
 const CommentBox = (props, context) => (
-  <form>
-    <textarea placeholder={context.t("Add a comment...")} />
+  <form className={styles.commentBox}>
+    <Textarea
+      className={styles.input}
+      placeholder={context.t("Add a comment...")}
+    />
   </form>
 );
 
--- frontend/src/components/TimeStamp/index.js
+++ frontend/src/components/TimeStamp/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
-const TimeStamp=(props,context)=>props.time;
+const TimeStamp = (props, context) => (
+  <span className={styles.time}>{props.time}</span>
+);
 
-TimeStamp.propTypes={
-    time:PropTypes.string.isRequired
+TimeStamp.propTypes = {
+  time: PropTypes.string.isRequired
+};
+
+TimeStamp.contextTypes = {
+  t: PropTypes.func.isRequired
 };
 
 export default TimeStamp;
