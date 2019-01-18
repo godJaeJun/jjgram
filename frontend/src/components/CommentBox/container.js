0735d4dbf9440737a5840ff3d3f628b227ee83d9
이재준 <asapz@naver.com>
2019-01-18 09:39:47 +0900

    Controlling Comment

--- frontend/src/components/CommentBox/container.js
+++ frontend/src/components/CommentBox/container.js
@@ -2,8 +2,26 @@ import React, { Component } from "react";
 import CommentBox from "./presenter";
 
 class Container extends Component {
+  state={
+    comment:""
+  }
   render() {
-    return <CommentBox />;
+    return <CommentBox 
+    {...this.state}
+    handleInputChange={this._handleInputChange}
+    handleKeyPress={this._handleKeyPress}/>;
+  }
+  _handleInputChange=event=>{
+    const {target :{value}}=event;
+    this.setState({
+      comment:value
+    });
+  };
+  _handleKeyPress=event=>{
+    const {key}=event;
+    if(key==="Enter"){
+      event.preventDefault();//Enter 무효화 자바스크립트에게 디폴트행동을 하지 말라고 한다.
+    }
   }
 }
 
--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -8,6 +8,9 @@ const CommentBox = (props, context) => (
     <Textarea
       className={styles.input}
       placeholder={context.t("Add a comment...")}
+      value={props.comment}
+      onChange={props.handleInputChange}
+      onKeyPress={props.handleKeyPress}
     />
   </form>
 );
@@ -15,5 +18,11 @@ const CommentBox = (props, context) => (
 CommentBox.contextTypes = {
   t: PropTypes.func.isRequired
 };
+
+CommentBox.propTypes={
+  handleInputChange:PropTypes.func.isRequired,
+  handleKeyPress:PropTypes.func.isRequired,
+  comment:PropTypes.string.isRequired
+}
 
 export default CommentBox;
